bda22522ddf1b1ae0e3b141987de95f66ec63be1
Pedro Marques <pdro.marqs2015@hotmail.com>
2023-05-09 21:31:48 -0300

    inicio area de comentarios

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.Comment}>
+            <img src="https://github.com/Pdro-marqss.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Pedro Marques</strong>
+                            <time title='11 de Maio ás 08:13h' dateTime='2023-05-11 00:13:30'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Pedro, parabéns!! 👏🏼👏🏼</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp size={20} />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
