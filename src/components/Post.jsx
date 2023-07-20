15fe017996e08f58cb004ef0188b337b1de58115
Pedro Marques <pdro.marqs2015@hotmail.com>
2023-07-19 23:00:29 -0300

    validando formulario de comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,7 +32,12 @@ export function Post({ author, publishedAt, content }) {
 	}
 
 	function handleNewCommentChange() {
+		event.target.setCustomValidity('');
 		setNewCommentText(event.target.value);
+	}
+
+	function handleNewCommentInvalid() {
+		event.target.setCustomValidity('Esse campo é obrigatório!')
 	}
 
 	function deleteComment(commentToDelete) {
@@ -42,6 +47,9 @@ export function Post({ author, publishedAt, content }) {
 
 		setComments(commentsWithoutDeletedOne)
 	}
+
+
+	const isNewCommentEmpty = newCommentText.length === 0;
 
 	return (
 		<article className={styles.post}>
@@ -77,10 +85,12 @@ export function Post({ author, publishedAt, content }) {
 					placeholder='Deixe um comentário'
 					value={newCommentText}
 					onChange={handleNewCommentChange}
+					onInvalid={handleNewCommentInvalid}
+					required
 				/>
 
 				<footer>
-					<button type='submite'>Publicar</button>
+					<button type='submite' disabled={isNewCommentEmpty}>Publicar</button>
 				</footer>
 			</form>
 
